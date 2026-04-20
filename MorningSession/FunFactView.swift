de673c46e658214d5f53dce47795b82f60c514ce
Fuad Agus Salim <fuadagussalim@mail.ugm.ac.id>
2026-04-20 11:48:09 +0700

    day 1 - get to know to xcode

--- MorningSession/FunFactView.swift
+++ MorningSession/FunFactView.swift
@@ -9,8 +9,8 @@ import SwiftUI
 
 struct FunFactView: View {
     var body: some View {
-        Image("me").resizable().aspectRatio(contentMode: .fit).frame(width: 150, height: 150)
-        Text("Kadang dikira orang Maluku, kadang Flores, orang Lanto")
+        Image("me").resizable().aspectRatio(contentMode: .fit).frame(width: 150, height: 150).border(Color.black, width: 10).cornerRadius(20)
+        Text("Kadang dikira orang ambon, kadang flores, kadang ngaku batak, papua belum pernah").multilineTextAlignment(.center).fontWeight(.bold)
     }
 }
 
